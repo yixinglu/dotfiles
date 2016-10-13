7e38241bc197ee84656cde6534ed5c370aafdd90
yixinglu <yxlyee@gmail.com>
2016-10-13 13:23:24 +0800

    replace home path

--- zsh/custom/env.zsh
+++ zsh/custom/env.zsh
@@ -3,8 +3,8 @@ CLION_VERSION=2016.2.1
 IDEA_VERSION=145.972.3
 
 # clion
-PATH="$PATH:/home/yee/tools/clion-$CLION_VERSION/bin"
+PATH="$PATH:$HOME/tools/clion-$CLION_VERSION/bin"
 # IntelliJ
-PATH="$PATH:/home/yee/tools/idea-IC-$IDEA_VERSION/bin"
+PATH="$PATH:$HOME/tools/idea-IC-$IDEA_VERSION/bin"
 
 export PATH
