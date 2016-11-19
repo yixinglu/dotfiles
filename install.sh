95935bdb793107bbb1bbf06609a520f5289b1fd7
yixinglu <yxlyee@gmail.com>
2016-11-20 02:15:28 +0800

    use submodule to manage tpm

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ spacemacs
 gitconfig
 bash_aliases
 tmux.conf
+tmux/plugins/tpm
 ssh/config
 clang-format
 zshrc
@@ -18,11 +19,6 @@ oh-my-zsh/custom/*.zsh
 if [[ ! -d ~/.oh-my-zsh ]]; then
     echo "install oh-my-zsh."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-if [[ ! -d ~/.tmux/plugins/tpm ]]; then
-    echo "install tmux plugin manager."
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 for file in $FILES; do
