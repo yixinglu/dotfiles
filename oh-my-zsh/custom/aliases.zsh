46642162edbf3d7555f3d3df05a240bdd9122398
yixinglu <yxlyee@gmail.com>
2016-12-06 14:18:01 +0800

    alias npm for zsh and increase font size

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,2 +1,6 @@
 alias g='git'
 alias go='gnome-open'
+alias cnpm='npm --registry=https://registry.npm.taobao.org \
+--cache=$HOME/.npm/.cache/cnpm \
+--disturl=https://npm.taobao.org/dist \
+--userconfig=$HOME/.cnpmrc'
