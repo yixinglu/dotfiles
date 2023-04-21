7783f715b50b9ed19b7dd85deed11e827e0aeaa3
yixinglu <2520865+yixinglu@users.noreply.github.com>
2023-04-21 23:53:21 +0800

    HOMEBREW update time

--- oh-my-zsh/custom/env.zsh
+++ oh-my-zsh/custom/env.zsh
@@ -2,6 +2,7 @@
 export HOMEBREW_BREW_GIT_REMOTE=https://mirrors.ustc.edu.cn/brew.git
 export HOMEBREW_CORE_GIT_REMOTE=https://mirrors.ustc.edu.cn/homebrew-core.git
 export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+export HOMEBREW_API_AUTO_UPDATE_SECS=86440
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
