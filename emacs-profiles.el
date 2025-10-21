bc90c1c835b60c57864bf0ba3bbf68d0b48a25bf
yixinglu <2520865+yixinglu@users.noreply.github.com>
2025-10-21 11:06:07 +0800

    emacs profiles

---
+++ emacs-profiles.el
@@ -0,0 +1,5 @@
+;; git clone https://github.com/plexus/chemacs2.git ~/.emacs.d
+;; ln -snf $PWD/emacs-profiles.el ~/.emacs-profiles.el
+(("default"   . ((user-emacs-directory . "~/.config/doomemacs")))
+ ("doom" . ((user-emacs-directory . "~/.config/doomemacs")))
+ ("spacemacs"   . ((user-emacs-directory . "~/.config/spacemacs"))))
