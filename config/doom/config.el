12c07b3e63b1f02414d2974409db7bbf7aff2a7f
Yee <2520865+yixinglu@users.noreply.github.com>
2025-10-23 02:32:26 +0800

    fix global center mode

--- config/doom/config.el
+++ config/doom/config.el
@@ -93,6 +93,10 @@
   (setq pyim-page-style 'two-lines)
   (setq pyim-page-tooltip '(posframe popup minibuffer)))
 
+(unless (boundp 'mouse-wheel-up-event)
+  (defvar mouse-wheel-up-event 'mouse-4))
+(unless (boundp 'mouse-wheel-down-event)
+  (defvar mouse-wheel-down-event 'mouse-5))
 (use-package! centered-cursor-mode
   :config
   (global-centered-cursor-mode))
