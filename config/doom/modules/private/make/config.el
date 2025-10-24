4b2c0fc3bfd8dccd163477b7fb7df02195c198bd
Yee <2520865+yixinglu@users.noreply.github.com>
2025-10-24 18:30:22 +0800

    feat(make): enhance project compilation with recompile and window toggle
    - Add make/project-recompile for repeating last compilation
    - Add make/show-hide-compilation-window to toggle compilation window
    - Refactor keybindings under leader prefix "c" for compilation actions
    - Improve process handling and user prompts during compilation

--- config/doom/modules/private/make/config.el
+++ config/doom/modules/private/make/config.el
@@ -1,12 +1,12 @@
 ;;; private/make/config.el -*- lexical-binding: t; -*-
 
-(defun make/project-root-name ()
+(defun make/project-root-name()
   "get project name"
   (if (projectile-project-root)
       (file-name-nondirectory (directory-file-name (projectile-project-root)))
     nil))
 
-(defun make/compilation-buffer-name ()
+(defun make/compilation-buffer-name()
   "compilation buffer name, such as *compilation*<project-root-name>"
   (interactive)
   (if (make/project-root-name)
@@ -18,18 +18,41 @@
   (and (projectile-project-root)
        (file-exists-p! (expand-file-name "Makefile" (projectile-project-root)))))
 
-(defun make/project-compile()
-  "compile project by makefile first and then projectile-project-compile"
+(defun make/show-hide-compilation-window()
+  "show or hide compilation window"
   (interactive)
   (let ((compilation-buf (get-buffer (make/compilation-buffer-name)))
         (compilation-win (get-buffer-window (make/compilation-buffer-name))))
     (if compilation-win
         (delete-window compilation-win)
-      (if (and compilation-buf (process-live-p (get-buffer-process compilation-buf)))
-          (display-buffer compilation-buf)
-        (if (make/project-has-makefile-p)
-            (makefile-executor-execute-target (expand-file-name "Makefile" (projectile-project-root)))
-          (projectile-compile-project))))))
+      (when compilation-buf
+        (display-buffer compilation-buf)))))
+
+(defun make/project-execute-or-repeat(b)
+  "execute makefile target in project root directory"
+  (if (make/project-has-makefile-p)
+      (if b (makefile-executor-execute-last nil)
+        (makefile-executor-execute-target (expand-file-name "Makefile" (projectile-project-root))))
+    (projectile-compile-project)))
+
+(defun make/project-compile()
+  "compile project by makefile first and then projectile-project-compile"
+  (interactive)
+  (let ((compilation-buf (get-buffer (make/compilation-buffer-name)))
+        (flag t))
+    (when (and compilation-buf (process-live-p (get-buffer-process compilation-buf)))
+      (let ((yorn (y-or-n-p "A compilation is running. Kill it? ")))
+        (if yorn
+            (interrupt-process (get-buffer-process compilation-buf))
+          (setq flag nil))))
+    (when flag
+      (make/project-execute-or-repeat nil))))
+
+(defun make/project-recompile()
+  "recompile project by makefile first and then projectile-project-recompile"
+  (interactive)
+  (make/project-kill-compilation)
+  (make/project-execute-or-repeat t))
 
 (defun make/project-kill-compilation ()
   "kill the compilation if exists"
@@ -48,6 +71,8 @@
   :defer t)
 
 (map! :leader
-      :prefix "p"
-      :desc "Compile project (with Makefile support)" :n "c" #'make/project-compile
-      :desc "Kill compilation (if running)" :n "k" #'make/project-kill-compilation)
+      :prefix "c"
+      :desc "Compile project" :n "c" #'make/project-compile
+      :desc "Recompile project" :n "r" #'make/project-recompile
+      :desc "Open compilation window" :n "d" #'make/show-hide-compilation-window
+      :desc "Kill compilation" :n "k" #'make/project-kill-compilation)
