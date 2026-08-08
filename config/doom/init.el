c1bc54c2751d1cf2dda15e208ca6f994192e2397
xinglu.yee <xinglu.yee@bytedance.com>
2026-08-08 14:31:27 +0800

    fix(tty): Disable OSC terminal support in Doom Emacs

--- config/doom/init.el
+++ config/doom/init.el
@@ -112,7 +112,7 @@
 
        :os
        (:if (featurep :system 'macos) macos)  ; improve compatibility with macOS
-       (tty +osc)              ; improve the terminal Emacs experience
+       tty                 ; improve the terminal Emacs experience
 
        :lang
        ;;ada               ; In strong typing we (blindly) trust
